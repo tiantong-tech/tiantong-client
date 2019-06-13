cf7346b5f3e471ba810ebf05ae9dade8c4706a61
delylaric <delylaric@gmail.com>
2019-06-13 11:04:51 +0800

    加入vue-router

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>This is Home Page</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
