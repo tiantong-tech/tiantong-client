abca2d895287d1f0010e4891c94c820d2302dc88
delylaric <delylaric@gmail.com>
2019-06-27 15:43:59 +0800

    feat(dataSource): 抽象数据源模块

---
+++ src/mixins/dataSource.js
@@ -0,0 +1,115 @@
+import axios from '@/providers/axios'
+
+/**
+ * 1. 调用 this.initialize(url)
+ */
+
+const getData = () => ({
+  url: '',
+  list: [],
+  data: {},
+  meta: {
+    page: 0,
+    page_size: 0,
+    total: 0
+  },
+  params: {
+    page: 1,
+    page_size: 15,
+    search: ''
+  },
+  isLoading: false,
+})
+
+const computed = {
+  item () {
+    const id = this.$route.params.id
+
+    return id && this.data[id]
+  },
+  items () {
+    return this.list.map(id => this.data[id])
+  },
+  isAllSelected () {
+    return this.items.every(item => item.$selected) && this.items.length
+  },
+  isNoneSelected () {
+    return !this.items.some(item => item.$selected)
+  },
+  selectedStatus () {
+    if (this.isAllSelected) return true
+    else if (this.isNoneSelected) return false
+    else return 'minus'
+  },
+  selectedIds () {
+    return this.items.filter(item => item.$selected)
+      .map(item => item.id)
+  }
+}
+
+const methods = {
+  getDataSource () {
+    const handleBefore = () => {
+      this.list = []
+      this.data = {}
+      this.isLoading = true
+    }
+    const handleThen = response => {
+      const { page, page_size, total, data } = response.data
+      this.meta.page = page
+      this.meta.pageSize = page_size
+      this.meta.total = total
+      data.forEach(item => {
+        item.$selected = false
+        this.list.push(item.id)
+        this.$set(this.data, item.id, item)
+      })
+    }
+    const handleCatch = error => {
+      console.log(error)
+    }
+    const handleFinally = () => {
+      this.isLoading = false
+    }
+
+    handleBefore()
+    axios.post(this.url, this.params)
+      .then(handleThen)
+      .catch(handleCatch)
+      .finally(handleFinally)
+  },
+  initialize (url = '') {
+    this.url = url
+    this.getDataSource()
+  },
+  refresh () {
+    Object.assign(this.$data, getData())
+    this.getDataSource()
+  },
+  search () {
+    this.params.page = 1
+    this.getDataSource()
+  },
+  changePage (page) {
+    this.params.page = page
+    this.getDataSource()
+  },
+  changePageSize (pageSize) {
+    this.params.page_size = pageSize
+    this.getDataSource()
+  },
+  handleSelect (id) {
+    this.data[id].$selected = !this.data[id].$selected
+  },
+  handleSelectAll () {
+    const value = this.selectedStatus === true ? false : true
+
+    this.items.forEach(item => item.$selected = value)
+  }
+}
+
+export default {
+  data: getData,
+  computed,
+  methods,
+}
